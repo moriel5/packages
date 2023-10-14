f653b7a7a7ab975218177856be716df87b1e8260
Tracey Clark <tclark77@tlcnet.info>
2023-10-13 19:59:44 -0500

    Update completions

--- common/Scripts/helpers.zsh
+++ common/Scripts/helpers.zsh
@@ -1,23 +1,25 @@
 #!/usr/bin/env zsh
 
+autoload -U +X compinit && compinit
+autoload -U +X bashcompinit && bashcompinit
+
 # Goes to the root directory of the solus packages
 # git repository from anywhere on the filesystem.
 # This function will only work if this script is sourced
 # by your zsh shell.
-
 function gotosoluspkgs() {
     SCRIPT_PATH=$functions_source[gotosoluspkgs]
-    pushd $(dirname $(readlink "${SCRIPT_PATH}"))/../../
+    cd $(dirname $(readlink "${SCRIPT_PATH}"))/../../
 }
 
 # Goes to the root directory of the git repository
 function goroot() {
-    pushd $(git rev-parse --show-toplevel)
+    cd $(git rev-parse --show-toplevel)
 }
 
-# Push into a package directory
+# Change into a package directory
 function gotopkg() {
-    pushd $(git rev-parse --show-toplevel)/packages/*/$1
+    cd $(git rev-parse --show-toplevel)/packages/*/$1
 }
 
 # What provides a lib
@@ -30,31 +32,21 @@ function whatuses() {
     grep $1 $(git rev-parse --show-toplevel)/packages/*/*/abi_used_libs
 }
 
-# package name completion
-_gotopkg() {
-
+# Package name completion
+_gotopkg()
+{
     _list=$(ls $(git rev-parse --show-toplevel)/packages/*/)
-    # echo "List $_list"
-    local cur
-    cur=${words[-1]} # gets typed input
-    # echo "cur $cur"
 
-    # compadd -V 'Result' -- "${_list[@]}";
-    compadd -V "${_list[@]}" -- ${cur};
+    local cur prev
+    COMPREPLY=()
+    cur=${COMP_WORDS[COMP_CWORD]}
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-    # local cur prev
-    # COMPREPLY=()
-    # cur=${COMP_WORDS[COMP_CWORD]}
-    # prev="${COMP_WORDS[COMP_CWORD-1]}"
-    # echo "cur $cur  prev $prev"
-
-    # if [[ $COMP_CWORD -eq 1 ]] ; then
-    #     COMPREPLY=( $(compgen -W "${_list}" -- ${cur}) )
-    #     return 0
-    # fi
-    
-
+    if [[ $COMP_CWORD -eq 1 ]] ; then
+        COMPREPLY=( $(compgen -W "${_list}" -- ${cur}) )
+        return 0
+    fi
 }
 
-compdef _gotopkg gotopkg
+complete -F _gotopkg gotopkg
 
